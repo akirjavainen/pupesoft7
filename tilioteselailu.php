44a70d2fc602bab301caa208b02acdc87a6c4885
Antti Kirjavainen <antti.kirjavainen@gmail.com>
2019-12-07 01:08:27 +0200

    modified:   tilioteselailu.php

--- tilioteselailu.php
+++ tilioteselailu.php
@@ -2,7 +2,7 @@
 
 if (isset($_POST["tee"])) {
   if ($_POST["tee"] == 'lataa_tiedosto') $lataa_tiedosto=1;
-  if (!isset($_POST["kaunisnimi"])) $_POST["kaunisnimi"] = "";
+  if (!isset($_POST["kaunisnimi"])) $_POST["kaunisnimi"] = ""; // MODIFIED, added BUGFIX
   if ($_POST["kaunisnimi"] != '') $_POST["kaunisnimi"] = str_replace("/", "", $_POST["kaunisnimi"]);
 }
 
