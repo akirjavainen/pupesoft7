981a0e94471e28229ec4b5e07da2677e00605d59
Antti Kirjavainen <antti.kirjavainen@gmail.com>
2019-12-12 08:49:44 +0200

    modified:   raportit/asiakkaan_tilaukset_tuotteittain.php

--- raportit/asiakkaan_tilaukset_tuotteittain.php
+++ raportit/asiakkaan_tilaukset_tuotteittain.php
@@ -4,6 +4,7 @@
 $useslave = 1;
 
 if (isset($_POST["tee"])) {
+  if (!isset($_POST["kaunisnimi"])) $_POST["kaunisnimi"] = ""; // MODIFIED, added BUGFIX
   if ($_POST["tee"] == 'lataa_tiedosto') $lataa_tiedosto=1;
   if ($_POST["kaunisnimi"] != '') $_POST["kaunisnimi"] = str_replace("/", "", $_POST["kaunisnimi"]);
 }
