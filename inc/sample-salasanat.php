d8e6673ca7c8a9478a9836fb0e5eb2400cb57778
Antti Kirjavainen <antti.kirjavainen@gmail.com>
2020-10-09 13:26:30 +0300

    commit

--- inc/sample-salasanat.php
+++ inc/sample-salasanat.php
@@ -12,6 +12,10 @@ $unifaun_ps_user = 'APPORT KAYTTAJA';
 $unifaun_ps_pass = 'APPORT SALASANA';
 $unifaun_ps_path = '/in';
 
+// Pankki-integraatio:
+$sepa_pankkiyhteys_token = "";
+$sepa_pankkiyhteys_salasana = "";
+
 $palvelin = 'http://192.168.1.2/pupesoft/';
 
 if (isset($_SERVER['SERVER_PORT']) and $_SERVER['SERVER_PORT'] == '443') {
