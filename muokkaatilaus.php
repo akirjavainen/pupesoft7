359e35f9d134685b90f10fecf6bccf7e1a94fc3e
Antti Kirjavainen <antti.kirjavainen@gmail.com>
2021-02-09 15:38:13 +0200

    commit

--- muokkaatilaus.php
+++ muokkaatilaus.php
@@ -5,6 +5,7 @@ $useslave = 1;
 
 if (isset($_POST["tee"])) {
   if ($_POST["tee"] == 'lataa_tiedosto') $lataa_tiedosto=1;
+  if (!isset($_POST["kaunisnimi"])) $_POST["kaunisnimi"] = ""; // MODIFIED, BUGFIX
   if ($_POST["kaunisnimi"] != '') $_POST["kaunisnimi"] = str_replace("/", "", $_POST["kaunisnimi"]);
 }
 
