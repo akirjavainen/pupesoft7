14f9418a3972ebae0606e26638b6d35a39ca442b
Antti Kirjavainen <antti.kirjavainen@gmail.com>
2021-03-01 14:01:46 +0200

    commit

--- raportit/saatanat.php
+++ raportit/saatanat.php
@@ -652,7 +652,7 @@ if ($tee == 'NAYTA' or $eiliittymaa == 'ON') {
 
         for ($sa = 1; $sa < count($saatavat_array); $sa++) {
           if (!isset($saatavat_yhteensa[($saatavat_array[$sa-1]+1)."_".$saatavat_array[$sa]])) $saatavat_yhteensa[($saatavat_array[$sa-1]+1)."_".$saatavat_array[$sa]] = $row[($saatavat_array[$sa-1]+1)."_".$saatavat_array[$sa]];
-          else $saatavat_yhteensa[($saatavat_array[$sa-1]+1)."_".$saatavat_array[$sa]] += $row[($saatavat_array[$sa-1]+1)."_".$saatavat_array[$sa]];
+          else (float)$saatavat_yhteensa[($saatavat_array[$sa-1]+1)."_".$saatavat_array[$sa]] += (float)$row[($saatavat_array[$sa-1]+1)."_".$saatavat_array[$sa]]; // MUOKKAUS: BUGIKORJAUS (string + string)
         }
 
         if (!isset($saatavat_yhteensa["yli_{$saatavat_array[count($saatavat_array)-1]}"])) $saatavat_yhteensa["yli_{$saatavat_array[count($saatavat_array)-1]}"] = $row["yli_{$saatavat_array[count($saatavat_array)-1]}"];
