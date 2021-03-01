f6f2128d31cc7bc5175d253b9d6aa6f0aa21d266
Antti Kirjavainen <antti.kirjavainen@gmail.com>
2021-03-01 13:04:58 +0200

    commit

--- tilauskasittely/tilaus_myynti.php
+++ tilauskasittely/tilaus_myynti.php
@@ -40,7 +40,7 @@ if (isset($_REQUEST['ajax_popup'])) {
 }
 
 // MUOKKAUS: isset():
-foreach (array("perusta_tilaustyyppi", "projekti", "jarjlisa", "yt", "postitp", "kantaasiakastunnus", "ytunnus", "kateinen") as $v) {
+foreach (array("perusta_tilaustyyppi", "projekti", "jarjlisa", "yt", "postitp", "kantaasiakastunnus", "ytunnus", "kateinen", "projektilask") as $v) {
   if (!isset(${$v})) ${$v} = null;
 }
 
