63fd501ec613b9f45a801960ebf2525b05b6bbc7
Antti Kirjavainen <antti.kirjavainen@gmail.com>
2025-02-13 15:09:46 +0200

    commit

--- apix-api.php
+++ apix-api.php
@@ -47,7 +47,7 @@ function apix_receive($apix_keys) {
 
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $real_url);
-  curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+  //curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   $response = curl_exec($ch);
   curl_close($ch);
