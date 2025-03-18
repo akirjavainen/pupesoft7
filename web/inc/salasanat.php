f675aa9fc27f0782892074ea287d3e65bb9b4fdb
Jussi Jokinen <jussi.k.jokinen@gmail.com>
2025-03-18 10:35:05 +0200

    initial commit

---
+++ web/inc/salasanat.php
@@ -0,0 +1,14 @@
+<?php
+
+date_default_timezone_set('Europe/Helsinki');
+
+$dbhost = 'database';
+$dbuser = 'lamp';
+$dbpass = 'lamp';
+$dbkanta = 'lamp';
+
+$palvelin = 'https://pupesoft.lndo.site';
+
+// if ( isset( $_SERVER['SERVER_PORT'] && $_SERVER['SERVER_PORT'] == '443') ) {
+//     $palvelin = 'https://pupesoft.lndo.site';    
+// }
